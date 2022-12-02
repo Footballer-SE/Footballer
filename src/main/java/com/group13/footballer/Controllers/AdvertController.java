b140d160340ef10763d5efd24bd78a38224c6b8a
koray kahraman <koraykahraman92@hotmail.com>
2022-12-02 21:09:28 +0300

    Advert service,controller added

---
+++ src/main/java/com/group13/footballer/Controllers/AdvertController.java
@@ -0,0 +1,44 @@
+package com.group13.footballer.Controllers;
+
+
+import com.group13.footballer.Models.Advert;
+import com.group13.footballer.Services.AdvertService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class AdvertController {
+    private final AdvertService advertService;
+
+    public AdvertController(AdvertService advertService) {
+        this.advertService = advertService;
+    }
+
+    @PostMapping("/addAdvert")
+    public ResponseEntity<Advert> addAdvert(@RequestBody Advert advert) {
+        return advertService.addAdvert(advert);
+    }
+
+    @GetMapping("/AllAdverts")
+    public ResponseEntity<List<Advert>> getAllAdverts(){
+        return advertService.getAllAdverts();
+    }
+
+    @GetMapping("/getAdvert/{id}")
+    public ResponseEntity<Advert> getAdvertById(@PathVariable Long id){
+        return advertService.getAdvertById(id);
+    }
+    @PutMapping("/updateAdvert/{id}")
+    public ResponseEntity<Advert> updateAdvertById(@RequestBody Advert advert, @PathVariable Long id) {
+        return advertService.updateAdvertById(advert,id);
+    }
+    @DeleteMapping("/deleteAdvert/{id}")
+    public ResponseEntity deleteAdvertById(@PathVariable Long id) {
+        return advertService.deleteAdvertById(id);
+
+    }
+
+}
